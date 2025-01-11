120685492d3645c07553c10bcdbef56fa2700308
Reece <reece@Reeces-MacBook-Pro.local>
2025-01-11 23:36:08 +0000

    added terraform backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "s3ra-state-bucket"
-#     key            = "website/terraform.tfstate"
-#     region         = "eu-west-2"
-#     dynamodb_table = "s3ra_state_table"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "s3ra-state-bucket"
+    key            = "website/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "s3ra_state_table"
+  }
+}
